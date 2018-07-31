6296fac5fe0fcd896e2cb21d5f9902b695a075f8
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-08-01 08:05:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// // ServiceWorker登録：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('sw.js').then(function (registration) {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }).catch(function (err) {
+//         console.log('ServiceWorker registration failed: ', err);
+//     });
+// }
